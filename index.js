be01431730d269be163dd1e8bac8f8709232a75b
Christoph Obst <christoph.obst@posteo.de>
2023-02-23 10:52:01 +0100

    https://github.com/ChristophObst?tab=repositories

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // phew… not a lot going on here. Please add some code!
+
+const bookmarkButton = document.querySelector(".bookmark");
+
+bookmarkButton.addEventListener("click", () => {
+  console.log("hello");
+  bookmarkButton.classList.toggle("card__button-answer");
+});
