6de0a0a89fb6165303a7c1df05af4e90fdc06ef8
Christoph Obst <christoph.obst@posteo.de>
2023-02-23 11:35:30 +0100

    https://github.com/ChristophObst/Quiz-App-Template

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // phew… not a lot going on here. Please add some code!
 
 const bookmarkButton = document.querySelector(".bookmark");
+const answerButton = document.querySelector(".card__button-answer");
+const answer = document.querySelector(".card__answer"); 
 
-bookmarkButton.addEventListener("click", () => {
-  console.log("hello");
+bookmarkButton.addEventListener("click", () => {  
   bookmarkButton.classList.toggle("bookmark--active");
+});
+
+answerButton.addEventListener("click", () => {
+answer.classList.toggle("card__answer--active");
 });
